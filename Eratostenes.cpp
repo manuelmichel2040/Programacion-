b7eb0c65cba04f18a9de9890155c53beeb8e5d9b
manuelmichel2040 <56050630+manuelmichel2040@users.noreply.github.com>
2019-10-01 17:41:12 -0500

    actualizacion
    version final funcional

--- Eratostenes.cpp
+++ Eratostenes.cpp
@@ -1,1 +1,51 @@
 #include<iostream>
+#include<time.h>
+#include<stdlib.h>
+
+using namespace std;
+
+void llenar(int vec[],int );
+void mostrar(int vec[], int n);
+void coladera(int vec[], int);
+
+int main(){
+	int vec[1000];
+	
+	 llenar(vec, 1000);
+	 coladera(vec, 1000);
+	 mostrar(vec,1000);
+	 
+	
+	cout<<endl;
+	
+	
+}
+
+void llenar(int vec[],int n)
+{
+	for(int i=0;i<1000;i++){
+		vec[i]=1;
+	}
+	
+	
+}
+
+void coladera(int vec[], int)
+{
+		for(int i=2;i<1000;i++){
+		   if(vec[i])
+		      for(int j=2;i*j<1000;j++){
+			        vec[i*j]=0;
+		      }
+	  }
+	
+}
+
+void mostrar(int vec[], int n)
+{
+	 for(int i=2;i<1000;i++){
+	   if(vec[i])
+	      cout<<i<<" ";
+   }
+}
+
